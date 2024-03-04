4c164952913a6763e11a451f8565cb3270842fe7
sho7014 <shirasaka@ist.osaka-u.ac.jp>
2024-03-04 19:53:53 +0900

    modified:   SanoSawada.py

--- SanoSawada.py
+++ SanoSawada.py
@@ -1,27 +1,33 @@
 import numpy as np
 from dataclasses import dataclass, field
-from typing import Any, Tuple
+from typing import Any, List
 import matplotlib.pyplot as plt
 
 
 @dataclass
 class SanoSawada:
-    dim_prob: int
-    dim_recon: int
+    ts: np.ndarray
     num_neighbor: int
     dt: float
-    step_tau: int
-    step_td: int
+    step_jac: int
+    epsilon: float
+    td_idx: np.ndarray
+    sampling_neighbors: str = "sequential"
     model: Any = None
 
     def __post_init__(self):
-        self.tau = self.dt * self.step_tau
-        self.td = self.dt * self.step_td
+        self.l_end = np.abs(np.min(self.td_idx))
+        self.jac_len = (self.ts.shape[0]-1-self.step_jac-self.l_end)//self.step_jac + 1
+        self.r_end = (self.jac_len-1)*self.step_jac + self.l_end
 
-    def set_epsilon(self):
-        pass
+        acceptable_sampling_methods = ["sequential", "random"]
+        if self.sampling_neighbors not in acceptable_sampling_methods:
+            raise ValueError(f"{self.sampling_neighbors} is not an acceptable value")
 
-    def reconstruct_phase_space(self, embedding_dimension, delay):
+    def set_radius_of_ball_for_neighboring_points_search(self):
+        self.radius = self.epsilon*(np.max(self.ts)-np.min(self.ts))
+
+    def delay_embedding(self):
         # Phase space reconstruction (simplified example)
         pass
 
